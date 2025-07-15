6982d93292a8de6aac7e968bf0b6adfe9b7114e4
Anisa Budiarthati <40983364+nisbud@users.noreply.github.com>
2025-07-15 19:15:49 +0700

    init project

---
+++ tuny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tuny
+//
+//  Created by Anisa Budi Arthati on 15/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
