302f076a0f9aa042f637c202156c6330a50352df
Anisa Budiarthati <40983364+nisbud@users.noreply.github.com>
2025-07-17 00:38:35 +0700

    adjust song to be compatible to response

--- tuny/Models/Song.swift
+++ tuny/Models/Song.swift
@@ -12,13 +12,24 @@ struct Song {
 	let title: String
 	let artist: String
 	let album: String
+	let duration: Double
 	let artworkURL: URL?
 	
-	init(id: String, title: String, artist: String, album: String, artworkURL: URL? = nil) {
+	init(id: String, title: String, artist: String, album: String, duration: Double, artworkURL: URL? = nil) {
 		self.id = id
 		self.title = title
 		self.artist = artist
 		self.album = album
+		self.duration = duration
 		self.artworkURL = artworkURL
+	}
+	
+	init(from sound: FreesoundResponse) {
+		self.id = String(sound.id)
+		self.title = sound.name
+		self.artist = sound.username
+		self.album = sound.description
+		self.duration = sound.duration
+		self.artworkURL = URL(string: sound.images.waveformMedium ?? "")
 	}
 }
